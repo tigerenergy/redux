6af28b55770d4be65d8464284b169e906ecf0c8d
tigerenergy <kisoon0947@gmail.com>
2022-05-31 17:23:45 +0900

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import classes from './Header.module.css'
+import { authActions } from '../store/index'
 
 const Header = () => {
+  const dispatch = useDispatch()
+  const logoutHandler = () => {
+    dispatch(authActions.logout())
+  }
   const isAuth = useSelector((state) => state.auth.isAuthenticated)
   return (
     <>
@@ -17,7 +22,7 @@ const Header = () => {
                 <a href='/'>My Sales</a>
               </li>
               <li>
-                <button>Logout</button>
+                <button onClick={logoutHandler}>Logout</button>
               </li>
             </ul>
           </nav>
